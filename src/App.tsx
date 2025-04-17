92b9fb0f1b46fabbd18f33a1269b68bb095aa48b
Pedro Regis <pedromr2@al.insper.edu.br>
2025-04-17 08:19:09 -0300

    handout feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import styled from "styled-components"
+
+function App (){
+    return (
+        <DashboardStyle>
+            <IMGstyle src = "src/assets/imagens.png"></IMGstyle>
+
+            <H1Style> Handout Front-End </H1Style>
+            <H2Style> Introdução básica ao Front-End</H2Style>
+
+            <p>Atividade prática</p>
+            
 
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        </DashboardStyle>
+    )
 }
-
+  
 export default App
+
+
+const DashboardStyle = styled.div`
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+ width: 100%;
+ height: 100vh;
+ background-color: #e9e4e4;
+ font-family: Arial, sans-serif;
+`;
+const H1Style = styled.h1`
+    color: red;
+    text-align: center;
+`;
+
+const H2Style = styled.h2`
+    color: black;
+    text-align: center;
+`;
+const IMGstyle = styled.img`
+    width: 20%;
+    height: auto;
+    margin: 5rem auto;
+`;
+
